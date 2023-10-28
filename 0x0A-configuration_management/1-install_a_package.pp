dcbade14b0fe7ba45c7666017b0a8557f0fa90a8
naijatechgirle <kellyeluagu@gmail.com>
2023-10-27 22:05:08 -0700

    ensure flask is installed from pip3

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,12 @@
+# Install Flask version 2.1.0
+python::pip { 'Flask':
+  ensure  => '2.1.0',
+  python_version => '3.8',
+}
+
 # Install the package puppet-lint 2.5.0
 
 package { 'puppet-lint':
-    ensure   => '2.1.0',
+    ensure   => '2.5.0',
     provider => 'gem',
-    }
+}
